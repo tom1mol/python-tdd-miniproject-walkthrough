2bd995fc23cd73f6749653b1a299393c50b00a4a
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-11-13 12:07:10 +0000

    vending machine 1

--- vending_machine.py
+++ vending_machine.py
@@ -1,15 +1,14 @@
 from byotest import *       #import byotest framework  from byotest.py
 
-def get_change(amount):     #function get_change with amount(argument)
-    if amount == 0:                                                     #if statement protects existing tests
-        return []               #return empty list
+usd_coins =[100, 50, 25, 10, 5, 1]
+eur_coins = [100, 50, 20, 10, 5, 2, 1]
 
-    if amount in [100, 50, 20, 10, 5, 2, 1]:
-        return [amount]
+def get_change(amount, coins=eur_coins):     #function get_change with amount(argument)
+                            # =(equals) means if we don't supply the argument it will default to using eur_coins argument
         
     change = []                         #empty list called change
-    for coin in [100,50,20,10,5,2,1]:
-        if coin <= amount:              #if change <= value we pass in
+    for coin in coins:
+        while coin <= amount:   #as long as coin<= amount..carry on adding it. only when its not will it move on or rtn change
             amount -= coin              # deduct amount of coin..from amount sent in
             change.append(coin)         #add that on to our change
             
@@ -27,6 +26,8 @@ test_are_equal(get_change(50), [50])
 test_are_equal(get_change(100), [100])
 test_are_equal(get_change(3), [2,1])
 test_are_equal(get_change(7), [5, 2])
+test_are_equal(get_change(9), [5,2,2])
+test_are_equal(get_change(35, usd_coins),[25,10])
 
 
 print("All tests pass!")    #create all tests pass message
